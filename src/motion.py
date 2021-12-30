2e489390ca7435a931ddda4f06df17e5957b6edf
chihuanchiang <gagagugu130ex@gmail.com>
2021-12-30 15:22:01 +0800

    Add camera module

--- src/motion.py
+++ src/motion.py
@@ -1,18 +1,22 @@
 from gpiozero import MotionSensor
-from alarm import setOff
+from datetime import datetime
+from time import sleep
+from camera import capture
 
 def trigger():
-    # take a picture and send it to twilio or mitake
-    # use setOff as a dummy function here
-    print("motion detected")
-    setOff(17, 1);
+    timeStamp = datetime.now()
+    fileName = "%04d-%02d-%02d-%02d-%02d-%02d.jpg" % (timeStamp.year, timeStamp.month, timeStamp.day, timeStamp.hour, timeStamp.minute, timeStamp.second)
+    capture(fileName)
+    print("saved %s" % fileName)
 
 def main():
     pir = MotionSensor(4, queue_len=8)
-    pir.when_motion = trigger;
-    print("interrupt process to stop")
+    print("Monitoring motion. Interrupt process to stop...")
     while(True):
-        pass
+        pir.wait_for_motion()
+        trigger()
+        pir.wait_for_no_motion()
+        sleep(2)
 
 if __name__ == "__main__":
     main()
