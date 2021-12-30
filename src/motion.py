2933647c3cf8ba5db105097eadcb10b48c2dda2e
chihuanchiang <gagagugu130ex@gmail.com>
2021-12-30 23:41:10 +0800

    Adjust camera parameters

--- src/motion.py
+++ src/motion.py
@@ -4,7 +4,7 @@ from imgurpython import ImgurClient
 from gpiozero import MotionSensor
 from datetime import datetime
 from time import sleep
-from camera import capture
+from picamera import PiCamera
 
 twilio_account_sid = os.environ['TWILIO_ACCOUNT_SID']
 twilio_auth_token = os.environ['TWILIO_AUTH_TOKEN']
@@ -14,10 +14,10 @@ imgur_client_secret = os.environ['IMGUR_CLIENT_SECRET']
 imgur_client = ImgurClient(imgur_client_id, imgur_client_secret)
 
 
-def trigger():
+def trigger(camera):
     timeStamp = datetime.now()
     fileName = "%04d-%02d-%02d-%02d-%02d-%02d.jpg" % (timeStamp.year, timeStamp.month, timeStamp.day, timeStamp.hour, timeStamp.minute, timeStamp.second)
-    capture(fileName)
+    camera.capture(fileName)
     print("saved %s" % fileName)
     image =  imgur_client.upload_from_path(fileName)
     print("uploaded %s, you can find it here: %s" % (fileName, image['link']))
@@ -32,12 +32,14 @@ def trigger():
     print(message.sid)
 
 def main():
-    
-    pir = MotionSensor(4, queue_len=8)
+    camera = PiCamera()
+    camera.rotation = 180
+    camera.start_preview(alpha=200)
+    pir = MotionSensor(4, queue_len=20, sample_rate=100, threshold=0.7)
     print("Monitoring motion. Interrupt process to stop...")
     while(True):
         pir.wait_for_motion()
-        trigger()
+        trigger(camera)
         pir.wait_for_no_motion()
         sleep(2)
 
